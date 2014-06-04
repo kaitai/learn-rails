9d43da7bd4626b63de0414ca137449989477b41a
kaitai <kaisa.taipale@gmail.com>
2014-06-03 20:06:28 -0500

    Saving to spreadsheet

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,7 +7,8 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.new(secure_params)
     if @contact.valid?
-      #todo: save data
+      # save data to google drive
+      @contact.update_spreadsheet
       # todo: send message
       flash[:notice] = "Message sent from #{@contact.name}."
       redirect_to root_path
